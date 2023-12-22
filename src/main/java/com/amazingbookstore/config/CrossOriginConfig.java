5b81a1bf6cf2a3b369374d9ae02a2cf66d6c9105
ChetanDehane <chetanpdehane@gmail.com>
2023-12-23 00:44:51 +0530

    add cross origin config

---
+++ src/main/java/com/amazingbookstore/config/CrossOriginConfig.java
@@ -0,0 +1,12 @@
+package com.amazingbookstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossOriginConfig implements WebMvcConfigurer{
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+}
